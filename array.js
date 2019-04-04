ce572522c2e06ac266d73a55138840c402a06027
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2019-04-04 12:01:54 -0300

    BubbleSort síncrono e assíncrono

--- array.js
+++ array.js
@@ -5,7 +5,7 @@ export const generateArray = (length) => {
   return arr
 }
 
-export const bubbleSortArray = (arr) => {
+export const bubbleSortSync = (arr) => {
   for (let i = 0; i < arr.length; i++)
     for (let j = 0; j < arr.length; j++)
       if (!isNaN(arr[j]) && !isNaN(arr[j + 1]))
@@ -14,4 +14,16 @@ export const bubbleSortArray = (arr) => {
           arr[j + 1] = arr[j]
           arr[j] = temp
         }
+  return arr
+}
+
+export const bubbleSort = (arr) => {
+  return new Promise((resolve, reject) => {
+    try {
+      setTimeout(() => resolve(bubbleSortSync(arr)), 0);
+    } catch (ex) {
+      console.log(ex)
+      reject(ex)
+    }
+  })
 }
--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // const length = require('os').cpus().length
 
-import { generateArray, bubbleSortArray } from './array'
+import { generateArray, bubbleSort } from './array'
 const start = new Date()
-const arr = generateArray(50000)
-bubbleSortArray(arr)
-console.log(`${((new Date()).getTime() - start.getTime()) / 1000} segundos`)
+const length = 20000
+
+console.log('Starting...')
+
+bubbleSort(generateArray(length)).then(result => {
+  console.log(`1 - ${((new Date()).getTime() - start.getTime()) / 1000} segundos`)
+})
+
+bubbleSort(generateArray(length)).then(result => {
+  console.log(`2 - ${((new Date()).getTime() - start.getTime()) / 1000} segundos`)
+})
+
+bubbleSort(generateArray(length)).then(result => {
+  console.log(`3 - ${((new Date()).getTime() - start.getTime()) / 1000} segundos`)
+})
+
+bubbleSort(generateArray(length)).then(result => {
+  console.log(`4 - ${((new Date()).getTime() - start.getTime()) / 1000} segundos`)
+})
+
+console.log('End script!')
